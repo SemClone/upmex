4ef227308048e956ed13331f482650d5a9c1f007
Oscar V <oscarvalenzuelab@users.noreply.github.com>
2025-08-09 23:05:17 -0700

    fix: Suppress urllib3 LibreSSL warning on macOS
    - Add warning filter for NotOpenSSLWarning
    - Common issue on macOS with LibreSSL instead of OpenSSL
    - Warning doesn't affect functionality but clutters output

--- src/upmex/__init__.py
+++ src/upmex/__init__.py
@@ -4,6 +4,15 @@ __version__ = "0.2.0"
 __author__ = "Oscar Valenzuela B."
 __email__ = "oscar.valenzuela.b@gmail.com"
 
+# Suppress urllib3 SSL warning on macOS with LibreSSL
+import warnings
+try:
+    import urllib3
+    warnings.filterwarnings('ignore', category=urllib3.exceptions.NotOpenSSLWarning)
+except (ImportError, AttributeError):
+    # urllib3 not installed or doesn't have NotOpenSSLWarning
+    pass
+
 from .core.extractor import PackageExtractor
 from .core.models import PackageMetadata, LicenseInfo
 
